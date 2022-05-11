39fbafcd637ae772ace6e8889f1e1aedd9d18d9a
jAcee12 <jaydoncameron80@protonmail.com>
2022-05-11 15:23:12 +1000

    Changed to Java 11 from Java 17

--- src/main/java/com/spacejaam/itservicesportal/controller/HomeController.java
+++ src/main/java/com/spacejaam/itservicesportal/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.spacejaam.itservicesportal.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +13,13 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/")
 public class HomeController {
 
-  public static ModelAndView getLayout(final String title, final String page) {
-    ModelAndView modelAndView = new ModelAndView("__Layout");
-    modelAndView.addObject("title", title);
-    modelAndView.addObject("content", page);
-    return modelAndView;
-  }
-
   @GetMapping(value = "/")
-  public ModelAndView home() {
-    return getLayout("Home", "index");
+  public ModelAndView home(HttpServletRequest request) {
+//    if ((Client) )
+
+    ModelAndView modelAndView = new ModelAndView("__Layout");
+    modelAndView.addObject("title", "Home");
+    modelAndView.addObject("content", "index");
+    return modelAndView;
   }
 }
