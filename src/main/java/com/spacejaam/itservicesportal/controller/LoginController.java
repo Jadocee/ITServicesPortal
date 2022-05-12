62fc83250a5b9162b831396be77eca0b6001e996
jAcee12 <jaydoncameron80@protonmail.com>
2022-05-13 03:01:45 +1000

    Delivers model and view. Handles session. Adds Client object to session.

--- src/main/java/com/spacejaam/itservicesportal/controller/LoginController.java
+++ src/main/java/com/spacejaam/itservicesportal/controller/LoginController.java
@@ -1,22 +1,26 @@
 package com.spacejaam.itservicesportal.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import com.spacejaam.itservicesportal.bean.client.Client;
+import com.spacejaam.itservicesportal.bean.client.Role;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
  */
 @Controller
-@RequestMapping("/login")
 public class LoginController {
 
-    @GetMapping("/")
-    public ModelAndView display(HttpServletRequest request) {
-        ModelAndView modelAndView = Helper.getLayout("Login", "login");
-
+    @GetMapping(value = "/login")
+    public ModelAndView display(HttpSession session) {
+        final ModelAndView modelAndView = new ModelAndView("__Layout");
+        modelAndView.addObject("pageTitle", "Login");
+        modelAndView.addObject("pageName", "login");
+        Client client = new Client();
+        client.init("Jaydon", "Cameron", "j@e", "33", Role.USER);
+        session.setAttribute("client", client);
         return modelAndView;
     }
 
